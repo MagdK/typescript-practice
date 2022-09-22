59e0f68eec24f73476285c2a392541f48237727b
Magda Kovács <kovacsmagda@gmail.com>
2022-09-22 17:16:19 +0200

    Lesson 9 notes

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,31 +1,14 @@
-let greet: Function;
+type StringOrNum = string | number;
+type objWithName = {name: string, uid: StringOrNum}
 
-// greet = 'hello' // will throw me an error
-
-greet = () => {
-    console.log("hellooow");
+const logDetails = (uid: StringOrNum, item: string) => {
+    console.log(`${item} has a uid of ${uid}`);
 }
 
-// if I place a question mark in front (c parameter) of it, that means the parameter is optional
-// I can set up a default value too, like 10 below for C
-// question mark and default parameter is usually not used together
-const add = (a: number, b: number, c?: string | number) => {
-    console.log(a + b);
-    console.log(c); // it will throw undefined if no c parameter is added to the function below when it is called
+const greet = (user: objWithName) => {
+    console.log(`${user.name} says hello`);
 }
 
-/* const add2 = (a: number, b: number, c: string | number = 10) => {
-    console.log(a + b);
-    console.log(c); // it will throw undefined if no c parameter is added to the function below when it is called
-} */
-
-add(5, 10); // only accepts numbers
-
-
-// Return type of the result of the function goes automatically, we don't have to declear it
-const minus = (a: number, b: number) => {
-    return a - b;
+const greetAgain = (user: objWithName) => {
+    console.log(`${user.name} says hello again`);
 }
-
-let result = minus(6, 3); // the type of result will be a number - based on what type of data we return
-//  result = 'macska' // this won't work, because the type is set to number
