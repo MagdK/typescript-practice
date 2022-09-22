bcbab11965ea96ca355643a7c2310598cc63ffd4
Magda Kovács <kovacsmagda@gmail.com>
2022-09-22 17:05:35 +0200

    Lesson 8 notes - function basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,84 +1,24 @@
 "use strict";
-// const character = 'timothy';
-// console.log(character);
-// const inputs = document.querySelectorAll('input');
-// console.log(inputs);
-// inputs.forEach(input => {
-//   console.log(input);
-// });
-let cat = 'Dena';
-let age = 5;
-// cat = 15 // we get a squiguly line - only accepts string
-// age = 'five' // only accepts number
-/* const circ = (diameter: number) => {
-    return diameter * Math.PI
-}
-
-console.log(circ(8)); */
-// ARRAYS
-let names = ['Dena', 'Effi', 'Masa'];
-names.push('Cirmi');
-console.log(names);
-// names.push(5); // won't allow me to push other than string
-// names[0] = true // won't allow me to overwrite to a boolean
-let numbers = [20, 89, 7];
-// numbers.push('Kate'); // type is number, won't allow me to push a string to it
-let mixed = [4, 'Effi', true, 'Tiszaszentimre', 15];
-mixed.push(87);
-mixed.push('Szentimre');
-mixed[0] = 'Kertalja'; // won't complain, since it is a mixed array
-// OBJECTS
-let ninja = {
-    name: "mario",
-    belt: "black",
-    age: 30
+let greet;
+// greet = 'hello' // will throw me an error
+greet = () => {
+    console.log("hellooow");
 };
-ninja.age = 50;
-ninja.name = "Ryan";
-// ninja.belt = 1 // won't work
-// ninja = [] // won't work because it is an object type
-// ninja = 'Pinja' // same - can't transform it to a string
-// BUT I can create a new object, but if I leave one key- value pairs, it won't pass. It has to match in all fields
-ninja = {
-    name: 'Yoshi',
-    belt: 'yellow',
-    age: 60
+// if I place a question mark in front (c parameter) of it, that means the parameter is optional
+// I can set up a default value too, like 10 below for C
+// question mark and default parameter is usually not used together
+const add = (a, b, c) => {
+    console.log(a + b);
+    console.log(c); // it will throw undefined if no c parameter is added to the function below when it is called
 };
-// VIDEO 5
-//explicit type
-let character;
-let year;
-let isLoggedIn;
-year = 2022;
-isLoggedIn = false;
-// arrays
-let ninjas; // we define that ninjas is a string array
-// ninjas.push('mizu') // it won't work because it is undefined, no values there yet
-// if we add an empty array by default we can avoid this issue
-// union types - 1 of 2/3 types
-let mixedArr = [];
-// both works because the type is union and we have each type defined in the parenteses()
-mixedArr.push('hellow');
-mixedArr.push(3);
-// objects
-let ninjaOne;
-ninjaOne = { name: 'Dena', age: 5 };
-let ninjaTwo;
-ninjaTwo = { name: 'mario', age: 20, beltColor: 'Brown' }; // if we try to add one more key-value pair, it will throw an error
-// DYNAMIC type
-let dino = 25;
-dino = true;
-console.log(dino);
-dino = 'belo';
-console.log(dino);
-let mixedAr = [];
-mixedAr.push(true);
-mixedAr.push(45);
-mixedAr.push('luigi');
-console.log(mixedAr);
-// we can set an object type with any value
-let pancake;
-pancake = { name: 'nutella', pieces: 8 };
-console.log(pancake);
-pancake = { name: 8, pieces: 'kakao' };
-console.log(pancake);
+/* const add2 = (a: number, b: number, c: string | number = 10) => {
+    console.log(a + b);
+    console.log(c); // it will throw undefined if no c parameter is added to the function below when it is called
+} */
+add(5, 10); // only accepts numbers
+// Return type of the result of the function goes automatically, we don't have to declear it
+const minus = (a, b) => {
+    return a - b;
+};
+let result = minus(6, 3); // the type of result will be a number - based on what type of data we return
+//  result = 'macska' // this won't work, because the type is set to number
