03fa1a10d0b062746a47e471edcadaeef982e958
Magda Kovács <kovacsmagda@gmail.com>
2022-10-11 11:19:10 +0200

    Set up repo to use modules

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,47 @@
-const anchor = document.querySelector('a')!; // this value might be null because typscript does not know if there is an anchor tag during development. It doesn't have access to index.html
+class Invoice {
+    /* Long version to create the class - below you can see the shorthand for it
+    readonly client: string; // we can read it outside and inside the class, but we can not change it
+    private details: string; // access modifier 'private' was used --> we get an error down, because of it
+    public amount: number; // public word is an access modifier
 
-/* 
-What we can do if we want to use a piece of code, but typescript does not know if it exists but we are certain, we can use an exclamation mark to push that through 
-*/
+    constructor(c: string, d: string, a: number) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    } */
 
-// if(anchor) {
-//   console.log(anchor.href);
-// }
-console.log(anchor.href);
 
-// const form = document.querySelector('form')!; // const form HTMLFormElement
+    constructor(
+        readonly client: string, // if we don't have access modiviers in front, this solution won't work!!!!
+        private details: string,
+        public amount: number
+    ) {};
 
-// const form = document.querySelector('.new-item-form')!; // const form: Element - we are passing in a class, and a class can be any sort of html element, it doesn't know whether or not it is a form. 
+    format() {
+        // this.client = 'something else'; // throws an error, because it is read only
+        return `${this.client} owes ${this.amount}Pound for ${this.details}`;
+    };
+};
+
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+
+console.log(invOne, invTwo);
+
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+
+invoices.forEach(inv => {
+    // inv.client = 'something else'; // I get an error, because it is read only
+    console.log(inv.client, inv.amount, inv.format()); 
+});
 
 
 // To combat this we can use type casting!!!!
 const form = document.querySelector('.new-item-form') as HTMLFormElement; // const form: HTMLFormElement
 
-console.log(form.children);
+// console.log(form.children);
 
 // inputs
 const type = document.querySelector('#type') as HTMLSelectElement;
