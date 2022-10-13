abbcb7ee28c9f47e656aece3816d14e16d85ad06
Magda Kovács <kovacsmagda@gmail.com>
2022-10-13 13:18:43 +0200

    Add tuple

--- public/app.js
+++ public/app.js
@@ -38,13 +38,54 @@ const ul = document.querySelector('ul');
 const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault(); // this will prevent the page refreshing when we submit the form
+    let values;
+    values = [tofrom.value, details.value, amount.valueAsNumber];
     let doc;
     if (type.value === 'invoice') {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     }
     else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
     console.log(doc);
     list.render(doc, type.value, 'end');
 });
+// GENERICS
+const addUID = (obj) => {
+    let uid = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let docOne = addUID({ name: 'yosi', age: 50 }); // invoced it and passed in two key value pairs in the object
+console.log(docOne.age); // now we can access name, age, uid because of the <T>
+// ENUMS
+var ResourceType;
+(function (ResourceType) {
+    ResourceType[ResourceType["BOOK"] = 0] = "BOOK";
+    ResourceType[ResourceType["AUTHOR"] = 1] = "AUTHOR";
+    ResourceType[ResourceType["FILM"] = 2] = "FILM";
+    ResourceType[ResourceType["DIRECTOR"] = 3] = "DIRECTOR";
+    ResourceType[ResourceType["PERSON"] = 4] = "PERSON";
+})(ResourceType || (ResourceType = {}));
+const docThree = {
+    uid: 21,
+    resourceName: ResourceType.AUTHOR,
+    data: 'shaun'
+};
+const docFour = {
+    uid: 32,
+    resourceName: ResourceType.PERSON,
+    data: ['fish', 'chicken', 'milk']
+};
+console.log(docThree, docFour);
+// ARRAY - we can change mostly everything
+let arr = ['str', 45, true];
+arr[0] = false;
+arr[1] = 'yoshi';
+arr = [true, 30, 'effi'];
+// TUPLE
+let tup = ['ryu', 23, true];
+tup[0] = 'ken';
+tup[1] = 45;
+tup[2] = false;
+let student;
+student = ['John Lee', 563121];
--- src/app.ts
+++ src/app.ts
@@ -51,14 +51,77 @@ const list = new ListTemplate(ul)
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault(); // this will prevent the page refreshing when we submit the form
 
+    let values: [string, string, number];
+    values = [tofrom.value, details.value, amount.valueAsNumber];
+
     let doc: HasFormatter;
     if(type.value === 'invoice') {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber)
+        doc = new Invoice(...values)
     } else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)
+        doc = new Payment(...values)
     }
     console.log(doc);
 
     list.render(doc, type.value, 'end');
-    
 });
+
+
+// GENERICS
+
+const addUID = <T extends {name: string}>(obj: T) => { // T captures whatever item we pass in - we are capturing what type it is, and if it is an object, what properties it has
+    let uid = Math.floor(Math.random() * 100);
+    return {...obj, uid};
+};
+
+let docOne = addUID({name: 'yosi', age: 50}); // invoced it and passed in two key value pairs in the object
+
+console.log(docOne.age); // now we can access name, age, uid because of the <T>
+
+
+// ENUMS
+
+enum ResourceType { BOOK, AUTHOR, FILM, DIRECTOR, PERSON }
+
+
+
+// WITH INTERFACES
+// an interface defines how an object should look
+
+interface Resource<T> {
+    uid: number;
+    resourceName: ResourceType;
+    data: T;
+}
+
+const docThree: Resource<string> = {
+    uid: 21,
+    resourceName: ResourceType.AUTHOR,
+    data: 'shaun'
+}
+
+const docFour: Resource<string[]> = {
+	uid: 32,
+	resourceName: ResourceType.PERSON,
+	data: ['fish', 'chicken', 'milk']
+}
+
+console.log(docThree, docFour);
+
+// ARRAY - we can change mostly everything
+let arr = ['str', 45, true];
+arr[0] = false;
+arr[1] = 'yoshi';
+
+arr = [true, 30, 'effi'];
+
+
+// TUPLE
+
+let tup: [string, number, boolean] = ['ryu', 23, true];
+
+tup[0] = 'ken';
+tup[1] = 45;
+tup[2] = false;
+
+let student: [string, number];
+student = ['John Lee', 563121];
